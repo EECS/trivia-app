46bb837cc1d95020744b16b1e2919200c5a3ec37
Alexander Whan <whan.alexander@gmail.com>
2020-07-23 15:17:17 -0700

    Fleshed out mapping of quiz questions in QuizList component

--- App.js
+++ App.js
@@ -3,21 +3,19 @@ import QuizList from "./components/QuizList";
 import axios from "axios";
 
 function App() {
-    const [questions, setQuestions] = useState([{question: "Question 1"}, {question: "Question 2"}]);
+    const [questions, setQuestions] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         setLoading(true)
         let cancel; // initialize variable to hold cancel token
         let url = 'https://opentdb.com/api.php?amount=10&category=22&difficulty=medium';
-        axios.get(url, {
-            cancelToken: new axios.CancelToken(c => cancel = c) // Will cancel the old request if user requests another before the previous one finishes
-        }).then(res => {
+        axios.get(url).then(res => {
             setLoading(false);
             let questionsFromAPI = res.data.results
             console.log('RESULTS from API:', questionsFromAPI);
+            setQuestions(questionsFromAPI);
             // setQuestions([{question: "Question 1"}, {question: "Question 2"}]);
-            // setQuestions(questionsFromAPI);
             // res.data = the part we care about
             // res.data.results = the actual questions array
             // res.data.results.question = the actual question text
@@ -25,10 +23,8 @@ function App() {
             // res.data.results.type = type of question (multiple choice vs. true/false)
         }, [])
         // console.log('MY QUESTIONS: ', questions);
-        return () => cancel(); // see cancel note above
+        // return () => cancel(); // see cancel note above
     }, [])
-
-    // if (loading) return "Loading..."
 
     return (
         <div>
@@ -39,3 +35,7 @@ function App() {
 }
 
 export default App;
+
+// {
+//     cancelToken: new axios.CancelToken(c => cancel = c) // Will cancel the old request if user requests another before the previous one finishes
+// }

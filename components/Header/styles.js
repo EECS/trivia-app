84be31d7f06e6128f8daebcaa4f9b820db1d0587
EECS <jacobshnn@gmail.com>
2020-07-30 13:20:42 -0700

    Add cursor:pointer to header.

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
-export const LinkItem = styled.a`
+export const LinkItem = styled.a `
     border: blue;
     margin-right: 20px;
+    cursor: pointer;
 `;

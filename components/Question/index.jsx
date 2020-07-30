d12f61bd0252c5a6aa48486f8adefa63727733ec
Alexander Whan <whan.alexander@gmail.com>
2020-07-30 13:55:12 -0700

    Added unique key to buttons in Quesition component

--- components/Question/index.jsx
+++ components/Question/index.jsx
@@ -1,42 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import React from 'r'
-import Button from './styles';
+import Button from "./styles";
 
 //Util import
 import { shuffledArray } from "../../utilities/utils";
 
-
 const Question = ({
-    handleAnswer,
-    data: { question, correct_answer, incorrect_answers } }) => {
-    // Combines correct/incorrect answer arrays
-    const combinedArray = [correct_answer, ...incorrect_answers];
-    const [allAnswers, setAllAnswers] = useState([])
+  handleAnswer,
+  data: { question, correct_answer, incorrect_answers },
+}) => {
+  // Combines correct/incorrect answer arrays
+  const combinedArray = [correct_answer, ...incorrect_answers];
+  const [allAnswers, setAllAnswers] = useState([]);
 
-    useEffect(() => {
-        // Randomized array of correct/incorrect answers for rendering
-        setAllAnswers(shuffledArray(combinedArray));
-    }, [question])
+  useEffect(() => {
+    // Randomized array of correct/incorrect answers for rendering
+    setAllAnswers(shuffledArray(combinedArray));
+  }, [question]);
 
-    // Ternary checks to see if an answer is equal to the correct answer to give it a "correct" button
-    return (
-        <div>
-            <p>{question}</p>
+  // Ternary checks to see if an answer is equal to the correct answer to give it a "correct" button
+  return (
+    <div>
+      <p>{question}</p>
 
-            {allAnswers.map(answer => {
-                return (
-                    answer === correct_answer ?
-                        <Button style={{ border: '2px solid black' }} onClick={() => handleAnswer(answer)}>
-                            {correct_answer}
-                        </Button>
-                        :
-                        <Button onClick={() => handleAnswer(answer)}>{answer}</Button>
-                )
-            })}
-        </div>
-    )
-
-}
+      {allAnswers.map((answer) => {
+        return answer === correct_answer ? (
+          <Button
+            key={answer}
+            style={{ border: "2px solid black" }}
+            onClick={() => handleAnswer(answer)}
+          >
+            {correct_answer}
+          </Button>
+        ) : (
+          <Button key={answer} onClick={() => handleAnswer(answer)}>
+            {answer}
+          </Button>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Question;
 

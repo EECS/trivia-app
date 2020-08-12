47062f847d09a94d6adcd5efb56928c243177d41
EECS <jacobshnn@gmail.com>
2020-08-12 13:19:33 -0700

    Reorganized db structure in project.

--- back-ts/knexfile.ts
+++ back-ts/knexfile.ts
@@ -18,10 +18,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      directory: __dirname + "/migrations"
+      directory: __dirname + "/src/db/migrations"
     },
     seeds: {
-      directory: __dirname + "/seeds"
+      directory: __dirname + "/src/db/seeds"
     }
   },
 };

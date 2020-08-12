263c58b36ecb03176f3cad7a21d1848664ca5229
EECS <jacobshnn@gmail.com>
2020-08-12 16:28:14 -0700

    Fix route bug lacking /.

--- back-ts/src/api/routes/auth.ts
+++ back-ts/src/api/routes/auth.ts
@@ -6,8 +6,7 @@ const route = Router()
 export default (app: Router) => {
     app.use('/auth', route)
 
-    route.post("login", async (req: Request, res: Response, next: NextFunction) => {
-
+    route.post("/login", async (req: Request, res: Response, next: NextFunction) => {
         passport.authenticate('local', (err, user, info) => {
             if (err || !user) {
                 return res.sendStatus(404)
@@ -24,7 +23,7 @@ export default (app: Router) => {
         })(req, res, next);
     })
 
-    route.post("required", async (req, res, next) => {
+    route.post("/required", async (req, res, next) => {
         if (req.isAuthenticated()) {
             res.send('you hit the authentication endpoint\n and here it is')
         } else {

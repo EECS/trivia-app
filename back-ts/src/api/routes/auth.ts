38cf8354e548cb8c558efab66762cec13b147e63
EECS <jacobshnn@gmail.com>
2020-08-12 15:55:48 -0700

    Create auth api route.

---
+++ back-ts/src/api/routes/auth.ts
@@ -0,0 +1,34 @@
+import { Router, Request, Response, NextFunction } from "express";
+import passport from "passport";
+
+const route = Router()
+
+export default (app: Router) => {
+    app.use('/auth', route)
+
+    route.post("login", async (req: Request, res: Response, next: NextFunction) => {
+
+        passport.authenticate('local', (err, user, info) => {
+            if (err || !user) {
+                return res.sendStatus(404)
+            }
+
+            req.logIn(user, (loginErr) => {
+
+                if (err) {
+                    return next(loginErr)
+                }
+
+                return res.send('You were authenticated & logged in!\n');
+            })
+        })(req, res, next);
+    })
+
+    route.post("required", async (req, res, next) => {
+        if (req.isAuthenticated()) {
+            res.send('you hit the authentication endpoint\n and here it is')
+        } else {
+            res.sendStatus(403)
+        }
+    })
+}

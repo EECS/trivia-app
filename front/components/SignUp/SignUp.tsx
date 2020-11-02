9108c1d8959dea9dee373aa912727df9bfc23d8c
EECS <jacobshnn@gmail.com>
2020-11-02 11:49:11 -0800

    Refactor sign up component to auth store

--- front/components/SignUp/SignUp.tsx
+++ front/components/SignUp/SignUp.tsx
@@ -1,43 +1,62 @@
-import React, { useState } from 'react'
-import { SignUpUser } from '../../services/SignUp/service'
+import React, { useState } from "react";
+import { AuthContext } from "../../contexts/Auth";
 
 export const SignUp = () => {
-    const [password, setPassword] = useState("")
-    const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-    const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value)
-    }
-    const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
-    }
+  const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
 
-    const submitData = async () => {
-        const response = await SignUpUser({
-            email: email,
-            password: password
-        })
+  return (
+    <AuthContext.Consumer>
+      {(auth) => {
+        if (auth === null) {
+          return Error("Auth context should not be null.");
+        }
 
-        return response;
-    }
-
-    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const response =  submitData()
-        console.log(response)
-    }
-
-    const onClickSubmit = async () => {
-        const response = await submitData();
-        console.log(response)
-    }
-    return (
-        <form action="submit" onSubmit={onSubmit}>
+        return (
+          <form
+            action="submit"
+            onSubmit={async (e) => {
+              setLoading(true);
+              e.preventDefault();
+              const response = await auth.signUp(email, password);
+              setLoading(false);
+            }}
+          >
             <label htmlFor="email">Email:</label>
-            <input name="email" type="email" onChange={onEmailChange} value={email}/>
+            <input
+              name="email"
+              type="email"
+              onChange={onEmailChange}
+              value={email}
+            />
             <label htmlFor="password">Password</label>
-            <input name="password" type="password" value={password} onChange={onPasswordChange}/>
-            <button onClick={onClickSubmit}>Submit</button>
-        </form>
-    )
-}
+            <input
+              name="password"
+              type="password"
+              value={password}
+              onChange={onPasswordChange}
+            />
+            <button
+              disabled={isLoading}
+              onClick={async () => {
+                setLoading(true);
+                const response = await auth.signUp(email, password);
+                setLoading(false);
+              }}
+            >
+              Submit
+            </button>
+          </form>
+        );
+      }}
+    </AuthContext.Consumer>
+  );
+};
